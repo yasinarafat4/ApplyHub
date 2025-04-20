8df3d45561fdc0175e5f0b68ac376277ef33a975
yasinarafat4 <mdyasinarafathasib@gmail.com>
2025-04-20 15:11:13 +0600

    Styles Added

--- src/pages/Apply.jsx
+++ src/pages/Apply.jsx
@@ -41,7 +41,7 @@ const Apply = () => {
           </div>
           <button
             type="submit"
-            className="w-full bg-blue-600 text-white py-2 rounded-lg hover:bg-blue-700 transition duration-300"
+            className="cursor-pointer w-full bg-blue-600 text-white py-2 rounded-lg hover:bg-blue-700 transition duration-300"
           >
             Submit Application
           </button>
